45fdd2b0e56ef729e997cf9ef72e70b4960ebbb4
rvyav <rvyav@hotmail.com>
2019-10-07 19:37:53 -0400

    test login users views

--- users/tests_users/test_views.py
+++ users/tests_users/test_views.py
@@ -1,8 +1,32 @@
-# from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# class TestPage(TestCase):
-# 	def test_home_page_works(self):
-# 		response = self.client.get("/")
-# 		self.assertEqual(response.status_code, 200)
-# 		self.assertTemplateUsed(response. 'home.html')
-# 		self.assertContains(response, 'BookTime')
+class TestUsersViews(TestCase):
+	"""
+	Test User Login & Registration Views.
+	"""
+	def setUp(self):
+		self.client = Client()
+
+	def test_login_get(self):
+		"""
+		Test Login page.
+		"""
+		response = self.client.get('/login/')
+
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed(response, 'registration/login.html')
+
+	def test_login_post(self):
+		"""
+		Test Post request Login Views.
+		"""
+		data = {
+			'username': 'tom', 
+			'password': 'tom',
+			'email': 'tom@gmail.com'
+		}
+		response = self.client.post('/login/', data)
+
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed(response, 'registration/login.html')
